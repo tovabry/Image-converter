c3e96e9496b2aa665194520bf28f40f43e28d1b1
Tova <tova.bryngelsson@live.se>
2025-05-02 14:28:14 +0200

    image converter exception

---
+++ service/src/main/java/se/whispers/modules/service/imageconverter/ImageConverterException.java
@@ -0,0 +1,13 @@
+package se.whispers.modules.service.imageconverter;
+
+public class ImageConverterException extends Exception {
+    public ImageConverterException(String message) {
+        super(message);
+    }
+
+    public ImageConverterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
